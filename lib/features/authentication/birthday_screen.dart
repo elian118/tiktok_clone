a7480e0e013874491baa7168aa6309e0c6b080f4
elian <elian118@naver.com>
2023-02-07 14:40:28 +0900

    4.8 BirthdayScreen - challenge

--- lib/features/authentication/birthday_screen.dart
+++ lib/features/authentication/birthday_screen.dart
@@ -1,10 +1,131 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
+import 'package:tiktok_clone/features/onboarding/interests_screen.dart';
 
-class BirthdayScreen extends StatelessWidget {
+class BirthdayScreen extends StatefulWidget {
   const BirthdayScreen({Key? key}) : super(key: key);
 
   @override
+  State<BirthdayScreen> createState() => _BirthdayScreenState();
+}
+
+class _BirthdayScreenState extends State<BirthdayScreen> {
+  final TextEditingController _birthdayController = TextEditingController();
+
+  DateTime initDate = DateTime.now();
+  late DateTime minDate = DateTime(
+    initDate.year - 12,
+    initDate.month,
+    initDate.day,
+    initDate.hour,
+    initDate.minute,
+    initDate.second,
+    initDate.millisecond,
+    initDate.microsecond,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _setTextFieldDate(initDate);
+  }
+
+  @override
+  void dispose() {
+    // 다른 인스턴스가 빌드될 때 _birthdayController 제거(메모리 관리)
+    _birthdayController.dispose();
+    super.dispose(); // 첫 행으로 가도 상관 없지만, 로직 파악 편의 상 메모리 정리 후 맨 아래 행 위치 권장
+  }
+
+  void _onNextTap() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const InterestsScreen(),
+      ),
+    );
+  }
+
+  void _setTextFieldDate(DateTime date) {
+    // final textDate = date.toString().split(" ").first;
+    // _birthdayController.value = TextEditingValue(text: textDate);
+    // 날짜 포매팅
+    _birthdayController.value =
+        TextEditingValue(text: DateFormat('yyyy년 MM월 dd일').format(date));
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: const Text('Sign up'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(Sizes.size36),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Gaps.v40,
+            const Text(
+              "When's your birthday?",
+              style: TextStyle(
+                fontSize: Sizes.size24,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Gaps.v8,
+            const Text(
+              "Your birthday won't be shown publicly.",
+              style: TextStyle(
+                fontSize: Sizes.size16,
+                color: Colors.black54,
+              ),
+            ),
+            Gaps.v28,
+            TextField(
+              enabled: false, // 비활성화(포커스 무시)
+              // 컨트롤러
+              controller: _birthdayController,
+              // 포커스 상태 밑줄 색상을 바꾸려면 enabledBorder, focusedBorder 두 속성을 모두 설정해야 한다.
+              decoration: InputDecoration(
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade400, // 활성 상태 밑줄 색상 지정
+                  ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade400, // 포커스 상태 => 활성 상태와 동일 색상 유지
+                  ),
+                ),
+              ),
+              cursorColor: Theme.of(context).primaryColor,
+            ),
+            Gaps.v28,
+            GestureDetector(
+              onTap:
+                  _onNextTap, // 같은 위젯 안에만 있다면, () => _onNextTap(context)로 작성할 필요가 없다.
+              child: const FormButton(disabled: false),
+            ) // 위젯 추출 v.2
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          height: 300,
+          child: CupertinoDatePicker(
+            mode: CupertinoDatePickerMode.date,
+            initialDateTime: initDate,
+            minimumDate: minDate,
+            maximumDate: initDate,
+            onDateTimeChanged: _setTextFieldDate,
+          ),
+        ),
+      ),
+    );
   }
 }

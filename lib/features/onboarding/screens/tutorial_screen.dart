8f80f4c9820c2af8c2c78de94529fcc2e469b565
elian <elian118@naver.com>
2023-02-08 17:53:38 +0900

    5.3 AnimatedCrossFade

--- lib/features/onboarding/screens/tutorial_screen.dart
+++ lib/features/onboarding/screens/tutorial_screen.dart
@@ -1,107 +1,81 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/enums/direction.dart';
+import 'package:tiktok_clone/constants/enums/showing_page.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/onboarding/widgets/tutorial.dart';
 
-class TutorialScreen extends StatelessWidget {
+class TutorialScreen extends StatefulWidget {
   const TutorialScreen({Key? key}) : super(key: key);
+
+  @override
+  State<TutorialScreen> createState() => _TutorialScreenState();
+}
+
+class _TutorialScreenState extends State<TutorialScreen> {
+  Direction _direction = Direction.right;
+
+  ShowingPage _showingPage = ShowingPage.first;
+
+  void _onPanUpdate(DragUpdateDetails details) {
+    setState(() {
+      _direction = details.delta.dx > 0 ? Direction.right : Direction.left;
+    });
+    // 드래그 동작 시 오프셋 변화 확인 테스트 -> cf. Offset delta
+    // 오프셋 x좌표(details.delta.dx): 좌로 드래그 => 음수 | 우로 드래그 => 양수
+    // 오프셋 y좌표(details.delta.dy): 위로 드래그 => 음수 | 아래로 드래그 => 양수
+    print(details);
+  }
+
+  void _onPanEnd(DragEndDetails details) {
+    setState(() {
+      _showingPage =
+          _direction == Direction.left ? ShowingPage.second : ShowingPage.first;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     // DefaultTabController -> TabBarView, TabPageSelector 컨트롤러 일괄 설정
-    return DefaultTabController(
-      length: 3, // 탭 수 지정
+    // return const TabBarEx();
+    return GestureDetector(
+      onPanUpdate: _onPanUpdate, // 드래그
+      onPanEnd: _onPanEnd, // 드롭,
       child: Scaffold(
-        body: SafeArea(
-          // TabBarView 컨트롤러는 아래와 같이 설정한다.
-          // 1) controller 속성을 직접 설정
-          // 2) DefaultTabController 위젯을 화면 상단에 랩핑해 증손자(TabBarView)에게 컨트롤러를 주입
-          child: TabBarView(
-            // controller: ,
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: Sizes.size24),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Gaps.v52,
-                    Text(
-                      'Watch cool videos!',
-                      style: TextStyle(
-                        fontSize: Sizes.size40,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Gaps.v20,
-                    Text(
-                      'Videos are personalized for you based on what you watch, like, and share.',
-                      style: TextStyle(
-                        fontSize: Sizes.size20,
-                      ),
-                    ),
-                  ],
-                ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: Sizes.size24),
+          child: SafeArea(
+            child: AnimatedCrossFade(
+              firstChild: const Tutorial(
+                  mainText: 'Watch cool videos!',
+                  subText:
+                      'Videos are personalized for you based on what you watch, like, and share.'),
+              secondChild: const Tutorial(
+                mainText: 'Follow the rules!',
+                subText: 'Take care of one another! please.',
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: Sizes.size24),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Gaps.v52,
-                    Text(
-                      'Follow the rules!',
-                      style: TextStyle(
-                        fontSize: Sizes.size40,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Gaps.v20,
-                    Text(
-                      'Videos are personalized for you based on what you watch, like, and share.',
-                      style: TextStyle(
-                        fontSize: Sizes.size20,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: Sizes.size24),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Gaps.v52,
-                    Text(
-                      'Enjoy the ride!',
-                      style: TextStyle(
-                        fontSize: Sizes.size40,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Gaps.v20,
-                    Text(
-                      'Videos are personalized for you based on what you watch, like, and share.',
-                      style: TextStyle(
-                        fontSize: Sizes.size20,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+              // crossFadeState: CrossFadeState.showFirst,
+              crossFadeState: _showingPage == ShowingPage.first
+                  ? CrossFadeState.showFirst
+                  : CrossFadeState.showSecond,
+              duration: const Duration(milliseconds: 300),
+            ),
           ),
         ),
         bottomNavigationBar: BottomAppBar(
-          elevation: 0,
-          child: Container(
-            padding: const EdgeInsets.symmetric(vertical: Sizes.size48),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                TabPageSelector(
-                  color: Colors.white,
-                  selectedColor: Colors.black54,
-                )
-              ],
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: Sizes.size24,
+              horizontal: 24,
+            ),
+            child: AnimatedOpacity(
+              opacity: _showingPage == ShowingPage.first ? 0 : 1,
+              duration: const Duration(milliseconds: 300),
+              child: CupertinoButton(
+                onPressed: () {},
+                color: Theme.of(context).primaryColor,
+                child: const Text('Enter the app!'),
+              ),
             ),
           ),
         ),

b3458d30f2441f69847f36751b9f0c38a79eaf73
elian <elian118@naver.com>
2023-02-12 11:50:50 +0900

    8.3 Text Input Actions

--- lib/features/videos/widgets/video_comments.dart
+++ lib/features/videos/widgets/video_comments.dart
@@ -13,8 +13,23 @@ class VideoComments extends StatefulWidget {
 }
 
 class _VideoCommentsState extends State<VideoComments> {
+  bool _isWriting = false;
+
   void _onClosePressed() {
     Utils.navPop(context);
+  }
+
+  void _onStopWriting() {
+    Utils.focusout(context);
+    setState(() {
+      _isWriting = false;
+    });
+  }
+
+  void _onStartWriting() {
+    setState(() {
+      _isWriting = true;
+    });
   }
 
   @override
@@ -44,110 +59,156 @@ class _VideoCommentsState extends State<VideoComments> {
             ),
           ],
         ),
-        body: Stack(
-          children: [
-            ListView.separated(
-              padding: const EdgeInsets.symmetric(
-                vertical: Sizes.size8,
-                horizontal: Sizes.size16,
-              ),
-              separatorBuilder: (context, index) => Gaps.v20,
-              itemCount: 10,
-              itemBuilder: (context, index) => Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const CircleAvatar(
-                    radius: 18,
-                    foregroundColor: Colors.white,
-                    foregroundImage: NetworkImage(foregroundImage),
-                    child: Text('광회'),
-                  ),
-                  Gaps.h10,
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+        body: GestureDetector(
+          onTap: _onStopWriting,
+          child: Stack(
+            children: [
+              ListView.separated(
+                padding: const EdgeInsets.symmetric(
+                  vertical: Sizes.size8,
+                  horizontal: Sizes.size16,
+                ),
+                separatorBuilder: (context, index) => Gaps.v20,
+                itemCount: 10,
+                itemBuilder: (context, index) => Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const CircleAvatar(
+                      radius: 18,
+                      foregroundColor: Colors.white,
+                      foregroundImage: NetworkImage(foregroundImage),
+                      child: Text('광회'),
+                    ),
+                    Gaps.h10,
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '광회',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: Sizes.size14,
+                              color: Colors.grey.shade500,
+                            ),
+                          ),
+                          Gaps.v3,
+                          const Text(
+                            "That's not it l've seen the same thing but also in a cave. That's not it l've seen the same thing but also in a cave. That's not it l've seen the same thing but also in a cave",
+                          ),
+                        ],
+                      ),
+                    ),
+                    Gaps.h10,
+                    Column(
                       children: [
+                        FaIcon(
+                          FontAwesomeIcons.heart,
+                          size: Sizes.size20,
+                          color: Colors.grey.shade500,
+                        ),
+                        Gaps.v2,
                         Text(
-                          '광회',
+                          '52.2K',
                           style: TextStyle(
-                            fontWeight: FontWeight.w500,
-                            fontSize: Sizes.size14,
                             color: Colors.grey.shade500,
                           ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              // Positioned + ListView.separated 함께 사용하면 -> 키보드 인터페이스와 혼용 가능
+              Positioned(
+                bottom: 0, // 키보드 등장 시 키보드 바로 위에 붙어 이동
+                width: size.width, // 포지션 위젯 너비를 화면너비로 명시 -> 에러 방지
+                child: BottomAppBar(
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: Sizes.size16,
+                      vertical: Sizes.size10,
+                    ),
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 18,
+                          backgroundColor: Colors.grey.shade500,
+                          foregroundColor: Colors.white,
+                          foregroundImage: const NetworkImage(foregroundImage),
+                          child: const Text('광회'),
                         ),
-                        Gaps.v3,
-                        const Text(
-                          "That's not it l've seen the same thing but also in a cave. That's not it l've seen the same thing but also in a cave. That's not it l've seen the same thing but also in a cave",
+                        Gaps.h10,
+                        Expanded(
+                          child: SizedBox(
+                            height: Sizes.size44,
+                            child: TextField(
+                              onTap: _onStartWriting,
+                              // return 키 눌렀을 때 줄바꿈 허용 -> 단, minLines, maxLines 값 필요
+                              expands: true,
+                              minLines: null,
+                              maxLines: null,
+                              // 키보드 -> return 키를 줄바꿈 키로 사용
+                              textInputAction: TextInputAction.newline,
+                              cursorColor: Theme.of(context).primaryColor,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius:
+                                      BorderRadius.circular(Sizes.size12),
+                                  borderSide: BorderSide.none,
+                                ),
+                                filled: true,
+                                fillColor: Colors.grey.shade200,
+                                contentPadding: const EdgeInsets.symmetric(
+                                  horizontal: Sizes.size12,
+                                ),
+                                hintText: 'Add comment...',
+                                suffixIcon: Padding(
+                                  padding: const EdgeInsets.only(
+                                      right: Sizes.size14),
+                                  child: Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      FaIcon(
+                                        FontAwesomeIcons.at,
+                                        color: Colors.grey.shade900,
+                                      ),
+                                      Gaps.h14,
+                                      FaIcon(
+                                        FontAwesomeIcons.gift,
+                                        color: Colors.grey.shade900,
+                                      ),
+                                      Gaps.h14,
+                                      FaIcon(
+                                        FontAwesomeIcons.faceSmile,
+                                        color: Colors.grey.shade900,
+                                      ),
+                                      Gaps.h14,
+                                      if (_isWriting)
+                                        GestureDetector(
+                                          onTap: _onStopWriting,
+                                          child: FaIcon(
+                                            FontAwesomeIcons.circleArrowUp,
+                                            color:
+                                                Theme.of(context).primaryColor,
+                                          ),
+                                        ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
                         ),
                       ],
                     ),
                   ),
-                  Gaps.h10,
-                  Column(
-                    children: [
-                      FaIcon(
-                        FontAwesomeIcons.heart,
-                        size: Sizes.size20,
-                        color: Colors.grey.shade500,
-                      ),
-                      Gaps.v2,
-                      Text(
-                        '52.2K',
-                        style: TextStyle(
-                          color: Colors.grey.shade500,
-                        ),
-                      )
-                    ],
-                  )
-                ],
-              ),
-            ),
-            // Positioned + ListView.separated 함께 사용하면 -> 키보드 인터페이스와 혼용 가능
-            Positioned(
-              bottom: 0, // 키보드 등장 시 키보드 바로 위에 붙어 이동
-              width: size.width, // 포지션 위젯 너비를 화면너비로 명시 -> 에러 방지
-              child: BottomAppBar(
-                color: Colors.white,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: Sizes.size16,
-                    vertical: Sizes.size10,
-                  ),
-                  child: Row(
-                    children: [
-                      CircleAvatar(
-                        radius: 18,
-                        backgroundColor: Colors.grey.shade500,
-                        foregroundColor: Colors.white,
-                        foregroundImage: const NetworkImage(foregroundImage),
-                        child: const Text('광회'),
-                      ),
-                      Gaps.h10,
-                      Expanded(
-                        child: TextField(
-                          cursorColor: Theme.of(context).primaryColor,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(Sizes.size12),
-                              borderSide: BorderSide.none,
-                            ),
-                            filled: true,
-                            fillColor: Colors.grey.shade200,
-                            contentPadding: const EdgeInsets.symmetric(
-                              vertical: Sizes.size12,
-                              horizontal: Sizes.size12,
-                            ),
-                            hintText: 'Add comment...',
-                            // suffixIcon:
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );

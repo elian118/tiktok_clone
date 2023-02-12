a7cce27c09afe976c90a6e1ce53d7b2d7af24ecc
elian <elian118@naver.com>
2023-02-12 10:09:52 +0900

    8.1 Comments

--- lib/features/videos/widgets/video_comments.dart
+++ lib/features/videos/widgets/video_comments.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/utils/utils.dart';
 
@@ -38,8 +39,78 @@ class _VideoCommentsState extends State<VideoComments> {
           ],
         ),
         body: ListView.builder(
+          padding: const EdgeInsets.symmetric(
+            vertical: Sizes.size8,
+            horizontal: Sizes.size16,
+          ),
           itemCount: 10,
-          itemBuilder: (context, index) => const Text('comments'),
+          itemBuilder: (context, index) => Padding(
+            padding: const EdgeInsets.symmetric(vertical: Sizes.size10),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const CircleAvatar(
+                  radius: 18,
+                  child: Text('광회'),
+                ),
+                Gaps.h10,
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '광회',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: Sizes.size14,
+                          color: Colors.grey.shade500,
+                        ),
+                      ),
+                      Gaps.v3,
+                      const Text(
+                        "That's not it l've seen the same thing but also in a cave. That's not it l've seen the same thing but also in a cave. That's not it l've seen the same thing but also in a cave",
+                      ),
+                    ],
+                  ),
+                ),
+                Gaps.h10,
+                Column(
+                  children: [
+                    FaIcon(
+                      FontAwesomeIcons.heart,
+                      size: Sizes.size20,
+                      color: Colors.grey.shade500,
+                    ),
+                    Gaps.v2,
+                    Text(
+                      '52.2K',
+                      style: TextStyle(
+                        color: Colors.grey.shade500,
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: Sizes.size16,
+              vertical: Sizes.size10,
+            ),
+            child: Row(
+              children: const [
+                CircleAvatar(
+                  radius: 18,
+                  foregroundColor: Colors.white,
+                  child: Text('광회'),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );

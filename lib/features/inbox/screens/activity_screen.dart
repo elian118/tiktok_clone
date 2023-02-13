d4b947ac1a9c82c44569f822c6c83cff5ccbdaba
elian <elian118@naver.com>
2023-02-13 10:10:38 +0900

    10.2 Dismissible

--- lib/features/inbox/screens/activity_screen.dart
+++ lib/features/inbox/screens/activity_screen.dart
@@ -13,64 +13,95 @@ class ActivityScreen extends StatelessWidget {
         title: const Text('All activity'),
       ),
       body: ListView(
-        padding: const EdgeInsets.symmetric(horizontal: Sizes.size20),
+        // padding: const EdgeInsets.symmetric(horizontal: Sizes.size20),
         children: [
-          Text(
-            'New',
-            style: TextStyle(
-              fontSize: Sizes.size14,
-              color: Colors.grey.shade500,
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: Sizes.size16),
+            child: Text(
+              'New',
+              style: TextStyle(
+                fontSize: Sizes.size14,
+                color: Colors.grey.shade500,
+              ),
             ),
           ),
           Gaps.v14,
-          ListTile(
-            contentPadding: EdgeInsets.zero,
-            leading: Container(
-              width: Sizes.size52,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.white,
-                border: Border.all(
-                  color: Colors.grey.shade400,
-                  width: Sizes.size1,
-                ),
-              ),
-              child: const Center(
+          // Dismissible -> 자식을 옆으로 밀어 제거할 수 있는 기능 간단 적용
+          Dismissible(
+            key: const Key('x'),
+            background: Container(
+              alignment: Alignment.centerLeft,
+              color: Colors.green,
+              child: const Padding(
+                padding: EdgeInsets.only(left: Sizes.size10),
                 child: FaIcon(
-                  FontAwesomeIcons.bell,
-                  color: Colors.black,
+                  FontAwesomeIcons.checkDouble,
+                  color: Colors.white,
+                  size: Sizes.size32,
                 ),
               ),
             ),
-            title: RichText(
-              text: TextSpan(
-                text: "Accounts updates:",
-                // RichText 기본 스타일
-                style: const TextStyle(
-                  fontWeight: FontWeight.w600,
-                  color: Colors.black,
-                  fontSize: Sizes.size16,
+            secondaryBackground: Container(
+              alignment: Alignment.centerRight,
+              color: Colors.red,
+              child: const Padding(
+                padding: EdgeInsets.only(left: Sizes.size10),
+                child: FaIcon(
+                  FontAwesomeIcons.trashCan,
+                  color: Colors.white,
+                  size: Sizes.size32,
                 ),
-                children: [
-                  const TextSpan(
-                    text: ' Upload longer videos',
-                    style: TextStyle(
-                      fontWeight: FontWeight.normal,
-                    ),
-                  ),
-                  TextSpan(
-                    text: ' 1h',
-                    style: TextStyle(
-                      fontWeight: FontWeight.normal,
-                      color: Colors.grey.shade500,
-                    ),
-                  ),
-                ],
               ),
             ),
-            trailing: const FaIcon(
-              FontAwesomeIcons.chevronRight,
-              size: Sizes.size16,
+            child: ListTile(
+              // contentPadding: EdgeInsets.zero, // 콘텐츠 패딩 제거
+              leading: Container(
+                width: Sizes.size52,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+                  border: Border.all(
+                    color: Colors.grey.shade400,
+                    width: Sizes.size1,
+                  ),
+                ),
+                child: const Center(
+                  child: FaIcon(
+                    FontAwesomeIcons.bell,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              title: RichText(
+                text: TextSpan(
+                  text: "Accounts updates:",
+                  // RichText 기본 스타일
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black,
+                    fontSize: Sizes.size16,
+                  ),
+                  children: [
+                    const TextSpan(
+                      text: ' Upload longer videos',
+                      style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                    TextSpan(
+                      text: ' 1h',
+                      style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                        color: Colors.grey.shade500,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              trailing: const FaIcon(
+                FontAwesomeIcons.chevronRight,
+                size: Sizes.size16,
+              ),
             ),
           )
         ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
-import 'package:tiktok_clone/features/main_navigation/screens/main_navigation_screen.dart';
+import 'package:tiktok_clone/features/inbox/screens/activity_screen.dart';
 
 void main() {
   runApp(const TickTokApp());
@@ -35,7 +35,8 @@ class TickTokApp extends StatelessWidget {
               fontWeight: FontWeight.w600),
         ),
       ),
-      home: const MainNavigationScreen(),
+      home: const ActivityScreen(),
+      // home: const MainNavigationScreen(),
       // home: const InterestsScreen(),
     );
   }

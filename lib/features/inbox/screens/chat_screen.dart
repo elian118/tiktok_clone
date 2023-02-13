0dd05a813876292c178745a2cc1386de08968478
elian <elian118@naver.com>
2023-02-13 17:44:31 +0900

    11.3 AnimatedList part 2

--- lib/features/inbox/screens/chat_screen.dart
+++ lib/features/inbox/screens/chat_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/inbox/screens/chat_detail_screen.dart';
+import 'package:tiktok_clone/utils/utils.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -14,14 +16,67 @@ class _ChatScreenState extends State<ChatScreen> {
 
   final List<int> _items = [];
 
+  final Duration _duration = const Duration(milliseconds: 500);
+
   void _addItem() {
     if (_key.currentState != null) {
       _key.currentState!.insertItem(
         _items.length,
-        duration: const Duration(milliseconds: 500),
+        duration: _duration,
       );
       _items.add(_items.length); // 배열 뒤에 추가
     }
+  }
+
+  void _deleteItem(int index) {
+    if (_key.currentState != null) {
+      _key.currentState!.removeItem(
+        index,
+        // removeItem() -> 두 번째 매개변수 animation 자동 입력됨
+        // 아이템 제거 애니메이션
+        (context, animation) => SizeTransition(
+          sizeFactor: animation,
+          child: Container(
+            color: Colors.red,
+            child: _makeTile(index),
+          ),
+        ),
+        duration: _duration,
+      );
+      _items.removeAt(index);
+    }
+  }
+
+  ListTile _makeTile(int index) {
+    return ListTile(
+      onLongPress: () => _deleteItem(index),
+      onTap: () => Utils.navPush(context, const ChatDetailScreen()),
+      key: UniqueKey(), // 유니크 키 적용 => 정확한 요소별 매핑 완료 -> 화면에 보인다.
+      leading: const CircleAvatar(
+        radius: 30,
+        foregroundImage: NetworkImage(
+            'https://avatars.githubusercontent.com/u/11626327?v=4'),
+        child: Text('함담보이'),
+      ),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            'Crystal $index',
+            style: const TextStyle(fontWeight: FontWeight.w500),
+          ),
+          Text(
+            '2:16 PM',
+            style: TextStyle(
+              color: Colors.grey.shade500,
+              fontSize: Sizes.size12,
+            ),
+          ),
+        ],
+      ),
+      subtitle: const Text('Say hi to AntonioBM'),
+    );
   }
 
   @override
@@ -51,33 +106,7 @@ class _ChatScreenState extends State<ChatScreen> {
               scale: animation, // 스케일 커지며
               child: SizeTransition(
                 sizeFactor: animation, // 축 사이즈 증가
-                child: ListTile(
-                  key: UniqueKey(), // 유니크 키 적용 => 정확한 요소별 매핑 완료 -> 화면에 보인다.
-                  leading: const CircleAvatar(
-                    radius: 30,
-                    foregroundImage: NetworkImage(
-                        'https://avatars.githubusercontent.com/u/11626327?v=4'),
-                    child: Text('함담보이'),
-                  ),
-                  title: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Text(
-                        'Crystal $index',
-                        style: const TextStyle(fontWeight: FontWeight.w500),
-                      ),
-                      Text(
-                        '2:16 PM',
-                        style: TextStyle(
-                          color: Colors.grey.shade500,
-                          fontSize: Sizes.size12,
-                        ),
-                      ),
-                    ],
-                  ),
-                  subtitle: const Text('Say hi to AntonioBM'),
-                ),
+                child: _makeTile(index),
               ),
             ),
           );

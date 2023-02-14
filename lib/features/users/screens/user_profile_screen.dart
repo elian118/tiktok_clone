7d5aca2e4eb1afb5dbbc2c3cb323fa3ff41f9486
elian <elian118@naver.com>
2023-02-14 10:57:07 +0900

    12.1 CustomScrollView

--- lib/features/users/screens/user_profile_screen.dart
+++ lib/features/users/screens/user_profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/common/constants/rawData/discovers.dart';
 
 class UserProfileScreen extends StatefulWidget {
   const UserProfileScreen({Key? key}) : super(key: key);
@@ -10,6 +11,28 @@ class UserProfileScreen extends StatefulWidget {
 class _UserProfileScreenState extends State<UserProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView();
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          // title: const Text('Hello'),
+          floating: true, // 유동화: 스크롤 아래로 내리다 위로 다시 스크롤 하면 appBar 뜨도록 허용
+          stretch: true, // 잡아 끌어 늘리기 허용
+          pinned: true, // SliverAppBar 사이즈가 줄어도 타이틀 위젯 남기기(고정)
+          backgroundColor: Colors.teal,
+          elevation: 1,
+          collapsedHeight: 80, // 접혔을 때 최소 높이
+          expandedHeight: 200, // 펼쳤을 때 최대 높이
+          flexibleSpace: FlexibleSpaceBar(
+            // SliverAppBar > stretch: true 일때, 상세 애니메이션 옵션 추가
+            stretchModes: const [
+              StretchMode.blurBackground,
+              StretchMode.zoomBackground,
+            ],
+            background: Image.asset(discoveredImages[0], fit: BoxFit.cover),
+            title: const Text('Hello'), // 제목을 여기에 위치해 두면 애니메이션 적용됨
+          ),
+        ),
+      ],
+    );
   }
 }

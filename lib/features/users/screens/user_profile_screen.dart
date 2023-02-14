6f549ad8d73848914021ec10bd0ffd220e222427
elian <elian118@naver.com>
2023-02-14 11:33:04 +0900

    12.3 SilverGrid

--- lib/features/users/screens/user_profile_screen.dart
+++ lib/features/users/screens/user_profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/common/constants/rawData/discovers.dart';
+import 'package:tiktok_clone/common/constants/sizes.dart';
 
 class UserProfileScreen extends StatefulWidget {
   const UserProfileScreen({Key? key}) : super(key: key);
@@ -15,8 +16,8 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
       slivers: [
         SliverAppBar(
           // title: const Text('Hello'), // FlexibleSpaceBar > background 설정 시 미사용 권장
-          floating: true, // 유동화: 스크롤 내리는 도중 다시 스크롤 올리면 appBar 100% 다시 띄우도록 허용
-          snap: true, // floating 애니메이션이 부드럽지 않고 스냅을 때리듯 획획 재생되도록 설정
+          // floating: true, // 유동화: 스크롤 내리는 도중 다시 스크롤 올리면 appBar 100% 다시 띄우도록 허용
+          // snap: true, // floating 애니메이션이 부드럽지 않고 스냅을 때리듯 획획 재생되도록 설정
           stretch: true, // 앱바를 본래 높이보다 일시적 잡아 끌어 늘리기 허용
           pinned: true, // SliverAppBar 사이즈가 줄어도 타이틀 위젯 남기기(고정)
           backgroundColor: Colors.teal,
@@ -43,6 +44,23 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
             ),
           ),
           itemExtent: 100, // 요소 높이
+        ),
+        SliverGrid(
+          // SliverFixedExtentList > delegate 와 실행방식 동일
+          delegate: SliverChildBuilderDelegate(
+            childCount: 50,
+            (context, index) => Container(
+              color: Colors.amber[100 * (index % 9)],
+              child: Align(
+                  alignment: Alignment.center, child: Text('Item $index')),
+            ),
+          ),
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 100, // 자식별 최대 너비값
+            mainAxisSpacing: Sizes.size20,
+            crossAxisSpacing: Sizes.size20,
+            childAspectRatio: 1, // 자식 표시비율 -> Ex. 16:9 비율은 16 / 9
+          ),
         ),
       ],
     );

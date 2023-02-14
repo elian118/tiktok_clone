bd4a6dd2c971e645d12bbc018065c9cfbdb94eff
elian <elian118@naver.com>
2023-02-14 12:42:27 +0900

    12.5 VerticalDivider

--- lib/features/users/screens/user_profile_screen.dart
+++ lib/features/users/screens/user_profile_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:tiktok_clone/common/constants/rawData/discovers.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/common/constants/gaps.dart';
+import 'package:tiktok_clone/common/constants/rawData/foreground_image.dart';
 import 'package:tiktok_clone/common/constants/sizes.dart';
 
 class UserProfileScreen extends StatefulWidget {
@@ -15,109 +17,124 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
     return CustomScrollView(
       slivers: [
         SliverAppBar(
-          // title: const Text('Hello'), // FlexibleSpaceBar > background 설정 시 미사용 권장
-          // floating: true, // 유동화: 스크롤 내리는 도중 다시 스크롤 올리면 appBar 100% 다시 띄우도록 허용
-          // snap: true, // floating 애니메이션이 부드럽지 않고 스냅을 때리듯 획획 재생되도록 설정
-          stretch: true, // 앱바를 본래 높이보다 일시적 잡아 끌어 늘리기 허용
-          pinned: true, // SliverAppBar 사이즈가 줄어도 타이틀 위젯 남기기(고정)
-          backgroundColor: Colors.teal,
-          elevation: 1,
-          collapsedHeight: 80, // 접혔을 때 최소 높이
-          expandedHeight: 200, // 펼쳤을 때 최대 높이
-          flexibleSpace: FlexibleSpaceBar(
-            // SliverAppBar > stretch: true 일때, 상세 애니메이션 옵션 추가
-            stretchModes: const [
-              StretchMode.blurBackground,
-              StretchMode.zoomBackground,
-            ],
-            background: Image.asset(discoveredImages[0], fit: BoxFit.cover),
-            title: const Text('Hello'), // 제목을 여기에 위치해 두면 애니메이션 적용됨
-          ),
+          title: const Text('Profile'),
+          actions: [
+            IconButton(
+              onPressed: () => print('dd'),
+              icon: const FaIcon(FontAwesomeIcons.gear),
+            ),
+          ],
         ),
         SliverToBoxAdapter(
           child: Column(
-            children: const [
-              CircleAvatar(
-                backgroundColor: Colors.red,
-                radius: 20,
-              )
+            children: [
+              const CircleAvatar(
+                radius: 30,
+                foregroundColor: Colors.teal,
+                foregroundImage: NetworkImage(foregroundImage),
+                child: Text('광회'),
+              ),
+              Gaps.v20,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    '@광회',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: Sizes.size18,
+                    ),
+                  ),
+                  Gaps.h5,
+                  FaIcon(
+                    FontAwesomeIcons.solidCircleCheck,
+                    color: Colors.blue.shade500,
+                    size: Sizes.size16,
+                  ),
+                ],
+              ),
+              Gaps.v24,
+              SizedBox(
+                height: Sizes.size48, // VerticalDivider 표시를 위한 설정
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        const Text(
+                          '37',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: Sizes.size18,
+                          ),
+                        ),
+                        Gaps.v3,
+                        Text(
+                          'Following',
+                          style: TextStyle(
+                            color: Colors.grey.shade500,
+                          ),
+                        ),
+                      ],
+                    ),
+                    // 구분선은 부모의 높이와 너비 설정이 있을 때만 보임
+                    VerticalDivider(
+                      color: Colors.grey.shade400,
+                      thickness: Sizes.size1,
+                      width: Sizes.size32,
+                      indent: Sizes.size14,
+                      endIndent: Sizes.size14,
+                    ),
+                    Column(
+                      children: [
+                        const Text(
+                          '10.5M',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: Sizes.size18,
+                          ),
+                        ),
+                        Gaps.v3,
+                        Text(
+                          'Followers',
+                          style: TextStyle(
+                            color: Colors.grey.shade500,
+                          ),
+                        ),
+                      ],
+                    ),
+                    VerticalDivider(
+                      color: Colors.grey.shade400,
+                      thickness: Sizes.size1,
+                      width: Sizes.size32,
+                      indent: Sizes.size14,
+                      endIndent: Sizes.size14,
+                    ),
+                    Column(
+                      children: [
+                        const Text(
+                          '149.3M',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: Sizes.size18,
+                          ),
+                        ),
+                        Gaps.v3,
+                        Text(
+                          'Likes',
+                          style: TextStyle(
+                            color: Colors.grey.shade500,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
-        ),
-        SliverFixedExtentList(
-          delegate: SliverChildBuilderDelegate(
-            childCount: 50,
-            (context, index) => Container(
-              color: Colors.amber[100 * (index % 9)],
-              child: Align(
-                  alignment: Alignment.center, child: Text('Item $index')),
-            ),
-          ),
-          itemExtent: 100, // 요소 높이
-        ),
-        SliverPersistentHeader(
-          // SliverPersistentHeader > delegate : 다른 슬리버 위젯보다 복잡
-          //  SliverPersistentHeaderDelegate 상속 위젯에서 필수 추상메서드 4개를 모두 재정의해야 하기 때문
-          //  참고 -> 추상메서드 4종은 예외를 발생시키도록 기본 설정돼 있으니, 반환값을 재정의 안 하면 에러만 뜬다.
-          delegate: CustomDelegate(),
-          pinned: true, // 슬리버 스크롤 시 앱바 아래 고정
-          // SliverAppBar > pinned: true 설정이 없다면,
-          //  아래 SliverPersistentHeader > floating: true 설정은 자신의 영역부터 SliverAppBar 처럼 pinned 돼 작동한다.
-          // floating: true, // SliverAppBar > pinned: true 상태에서 스크롤 내리는 도중 다시 올리면 일부가 조금 보이는 형태를 취함
-        ),
-        SliverGrid(
-          // SliverFixedExtentList > delegate 와 실행방식 동일
-          delegate: SliverChildBuilderDelegate(
-            childCount: 50,
-            (context, index) => Container(
-              color: Colors.amber[100 * (index % 9)],
-              child: Align(
-                  alignment: Alignment.center, child: Text('Item $index')),
-            ),
-          ),
-          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 100, // 자식별 최대 너비값
-            mainAxisSpacing: Sizes.size20,
-            crossAxisSpacing: Sizes.size20,
-            childAspectRatio: 1, // 자식 표시비율 -> Ex. 16:9 비율은 16 / 9
-          ),
-        ),
+        )
       ],
     );
-  }
-}
-
-class CustomDelegate extends SliverPersistentHeaderDelegate {
-  @override
-  Widget build(
-      BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return Container(
-      color: Colors.indigo,
-      child: const FractionallySizedBox(
-        heightFactor: 1,
-        child: Center(
-          child: Text(
-            'Title!!!!!',
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  // 최대 확장 높이
-  @override
-  double get maxExtent => 150;
-
-  // 최소 축소 높이
-  @override
-  double get minExtent => 80;
-
-  @override
-  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
-    return false;
-    // throw UnimplementedError();
   }
 }
---
+++ lib/features/users/screens/user_profile_screen_concept.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/common/constants/rawData/discovers.dart';
+import 'package:tiktok_clone/common/constants/sizes.dart';
+
+// 슬리버 위젯의 기본 개념과 원리를 설명한 스크린 위젯 -> UserProfileScreen 대신 갈아끼면 확인 가능
+class UserProfileScreenConcept extends StatefulWidget {
+  const UserProfileScreenConcept({Key? key}) : super(key: key);
+
+  @override
+  State<UserProfileScreenConcept> createState() =>
+      _UserProfileScreenConceptState();
+}
+
+class _UserProfileScreenConceptState extends State<UserProfileScreenConcept> {
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          // title: const Text('Hello'), // FlexibleSpaceBar > background 설정 시 미사용 권장
+          // floating: true, // 유동화: 스크롤 내리는 도중 다시 스크롤 올리면 appBar 100% 다시 띄우도록 허용
+          // snap: true, // floating 애니메이션이 부드럽지 않고 스냅을 때리듯 획획 재생되도록 설정
+          stretch: true, // 앱바를 본래 높이보다 일시적 잡아 끌어 늘리기 허용
+          pinned: true, // SliverAppBar 사이즈가 줄어도 타이틀 위젯 남기기(고정)
+          backgroundColor: Colors.teal,
+          elevation: 1,
+          collapsedHeight: 80, // 접혔을 때 최소 높이
+          expandedHeight: 200, // 펼쳤을 때 최대 높이
+          flexibleSpace: FlexibleSpaceBar(
+            // SliverAppBar > stretch: true 일때, 상세 애니메이션 옵션 추가
+            stretchModes: const [
+              StretchMode.blurBackground,
+              StretchMode.zoomBackground,
+            ],
+            background: Image.asset(discoveredImages[0], fit: BoxFit.cover),
+            title: const Text('Hello'), // 제목을 여기에 위치해 두면 애니메이션 적용됨
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: Column(
+            children: const [
+              CircleAvatar(
+                backgroundColor: Colors.red,
+                radius: 20,
+              )
+            ],
+          ),
+        ),
+        SliverFixedExtentList(
+          delegate: SliverChildBuilderDelegate(
+            childCount: 50,
+            (context, index) => Container(
+              color: Colors.amber[100 * (index % 9)],
+              child: Align(
+                  alignment: Alignment.center, child: Text('Item $index')),
+            ),
+          ),
+          itemExtent: 100, // 요소 높이
+        ),
+        SliverPersistentHeader(
+          // SliverPersistentHeader > delegate : 다른 슬리버 위젯보다 복잡
+          //  SliverPersistentHeaderDelegate 상속 위젯에서 필수 추상메서드 4개를 모두 재정의해야 하기 때문
+          //  참고 -> 추상메서드 4종은 예외를 발생시키도록 기본 설정돼 있으니, 반환값을 재정의 안 하면 에러만 뜬다.
+          delegate: CustomDelegate(),
+          pinned: true, // 슬리버 스크롤 시 앱바 아래 고정
+          // SliverAppBar > pinned: true 설정이 없다면,
+          //  아래 SliverPersistentHeader > floating: true 설정은 자신의 영역부터 SliverAppBar 처럼 pinned 돼 작동한다.
+          // floating: true, // SliverAppBar > pinned: true 상태에서 스크롤 내리는 도중 다시 올리면 일부가 조금 보이는 형태를 취함
+        ),
+        SliverGrid(
+          // SliverFixedExtentList > delegate 와 실행방식 동일
+          delegate: SliverChildBuilderDelegate(
+            childCount: 50,
+            (context, index) => Container(
+              color: Colors.amber[100 * (index % 9)],
+              child: Align(
+                  alignment: Alignment.center, child: Text('Item $index')),
+            ),
+          ),
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 100, // 자식별 최대 너비값
+            mainAxisSpacing: Sizes.size20,
+            crossAxisSpacing: Sizes.size20,
+            childAspectRatio: 1, // 자식 표시비율 -> Ex. 16:9 비율은 16 / 9
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class CustomDelegate extends SliverPersistentHeaderDelegate {
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      color: Colors.indigo,
+      child: const FractionallySizedBox(
+        heightFactor: 1,
+        child: Center(
+          child: Text(
+            'Title!!!!!',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  // 최대 확장 높이
+  @override
+  double get maxExtent => 150;
+
+  // 최소 축소 높이
+  @override
+  double get minExtent => 80;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return false;
+    // throw UnimplementedError();
+  }
+}

14ca54de155fc080dd6ee86f5885dcac34bd5e51
elian <elian118@naver.com>
2023-02-15 23:34:31 +0900

    14.4 LayoutBuilder + 14.5 ConstraintsBox

--- lib/features/discover/screens/discover_screen.dart
+++ lib/features/discover/screens/discover_screen.dart
@@ -5,6 +5,7 @@ import 'package:tiktok_clone/common/constants/gaps.dart';
 import 'package:tiktok_clone/common/constants/rawData/discovers.dart';
 import 'package:tiktok_clone/common/constants/rawData/foreground_image.dart';
 import 'package:tiktok_clone/common/constants/sizes.dart';
+import 'package:tiktok_clone/features/discover/widgets/custom_search_button.dart';
 import 'package:tiktok_clone/utils/utils.dart';
 
 class DiscoverScreen extends StatefulWidget {
@@ -59,65 +60,18 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
         resizeToAvoidBottomInset: false,
         appBar: AppBar(
           elevation: 1, // 앱바와 바디 사이 구분선 효과
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              GestureDetector(
-                  onTap: _moveBack,
-                  child: const FaIcon(FontAwesomeIcons.chevronLeft)),
-              Expanded(
-                child: Container(
-                  padding: const EdgeInsets.symmetric(horizontal: Sizes.size18),
-                  height: Sizes.size44,
-                  child: TextField(
-                    controller: _textEditingController,
-                    onChanged: _onSearchChanged,
-                    onSubmitted: _onSearchSubmitted,
-                    cursorColor: Theme.of(context).primaryColor,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(Sizes.size5),
-                        borderSide: BorderSide.none,
-                      ),
-                      filled: true,
-                      fillColor: Colors.grey.shade200,
-                      contentPadding:
-                          const EdgeInsets.symmetric(horizontal: Sizes.size12),
-                      prefixIcon: Container(
-                        width: Sizes.size20,
-                        alignment: Alignment.center,
-                        child: const FaIcon(
-                          FontAwesomeIcons.magnifyingGlass,
-                          color: Colors.black,
-                          size: Sizes.size18,
-                        ),
-                      ),
-                      suffixIcon: Container(
-                        width: Sizes.size20,
-                        alignment: Alignment.centerRight,
-                        padding: const EdgeInsets.only(
-                          left: Sizes.size10,
-                          right: Sizes.size8,
-                        ),
-                        child: AnimatedOpacity(
-                          opacity: _isThereSearchValue ? 1 : 0,
-                          duration: const Duration(milliseconds: 200),
-                          child: GestureDetector(
-                            onTap: _onCloseIcon,
-                            child: FaIcon(
-                              FontAwesomeIcons.solidCircleXmark,
-                              color: Colors.grey.shade600,
-                              size: Sizes.size18,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              const FaIcon(FontAwesomeIcons.sliders),
-            ],
+          // ConstrainedBox -> 최대/최소 크기를 제한할 수 있는 박스
+          //  참고. Container -> constraints 에서 동일한 속성 설정 가능(기능 동일)
+          title: ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: Breakpoint.sm),
+            child: CustomSearchButton(
+              moveBack: _moveBack,
+              textEditingController: _textEditingController,
+              onSearchChanged: _onSearchChanged,
+              onSearchSubmitted: _onSearchSubmitted,
+              isThereSearchValue: _isThereSearchValue,
+              onCloseIcon: _onCloseIcon,
+            ),
           ),
           // 쿠퍼티노텍스트필드
           // title: CupertinoSearchTextField(
@@ -155,68 +109,73 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
                 mainAxisSpacing: Sizes.size10,
                 childAspectRatio: 9 / 20, // 그리드 비율
               ),
-              itemBuilder: (context, index) => Column(
-                children: [
-                  Container(
-                    clipBehavior: Clip.hardEdge,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(Sizes.size4),
-                    ),
-                    child: AspectRatio(
-                      aspectRatio: 9 / 16,
-                      child: FadeInImage.assetNetwork(
-                        fit: BoxFit.cover,
-                        placeholder: 'assets/images/windmill-7367963.jpg',
-                        image:
-                            "https://cdn.pixabay.com/photo/2023/01/24/13/23/viet-nam-7741017_960_720.jpg",
+              itemBuilder: (context, index) => LayoutBuilder(
+                builder: (context, constraints) => Column(
+                  children: [
+                    Container(
+                      clipBehavior: Clip.hardEdge,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(Sizes.size4),
+                      ),
+                      child: AspectRatio(
+                        aspectRatio: 9 / 16,
+                        child: FadeInImage.assetNetwork(
+                          fit: BoxFit.cover,
+                          placeholder: 'assets/images/windmill-7367963.jpg',
+                          image:
+                              "https://cdn.pixabay.com/photo/2023/01/24/13/23/viet-nam-7741017_960_720.jpg",
+                        ),
                       ),
                     ),
-                  ),
-                  Gaps.v10,
-                  const Text(
-                    "This is a very long caption for my tiktok that i'm upload just now currently.",
-                    style: TextStyle(
-                      fontSize: Sizes.size18,
-                      fontWeight: FontWeight.bold,
+                    Gaps.v10,
+                    const Text(
+                      "This is a very long caption for my tiktok that i'm upload just now currently.",
+                      style: TextStyle(
+                        fontSize: Sizes.size18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
                     ),
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  Gaps.v8,
-                  DefaultTextStyle(
-                    style: TextStyle(
-                      fontSize: Sizes.size14,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.grey.shade600,
-                    ),
-                    child: Row(
-                      children: [
-                        const CircleAvatar(
-                          radius: 15,
-                          foregroundImage: NetworkImage(foregroundImage),
-                        ),
-                        Gaps.h4,
-                        const Expanded(
-                          child: Text(
-                            'My avatar is going to be very long',
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
-                        Gaps.h4,
-                        FaIcon(
-                          FontAwesomeIcons.heart,
-                          size: Sizes.size16,
+                    Gaps.v8,
+                    // 레이아웃이 너무 작으면 세부 텍스트 생략
+                    if (constraints.maxWidth < 200 ||
+                        constraints.maxWidth > 250)
+                      DefaultTextStyle(
+                        style: TextStyle(
+                          fontSize: Sizes.size14,
+                          fontWeight: FontWeight.w600,
                           color: Colors.grey.shade600,
                         ),
-                        Gaps.h2,
-                        const Text(
-                          '2.5M',
-                        )
-                      ],
-                    ),
-                  )
-                ],
+                        child: Row(
+                          children: [
+                            const CircleAvatar(
+                              radius: 15,
+                              foregroundImage: NetworkImage(foregroundImage),
+                            ),
+                            Gaps.h4,
+                            const Expanded(
+                              child: Text(
+                                'My avatar is going to be very long',
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                            Gaps.h4,
+                            FaIcon(
+                              FontAwesomeIcons.heart,
+                              size: Sizes.size16,
+                              color: Colors.grey.shade600,
+                            ),
+                            Gaps.h2,
+                            const Text(
+                              '2.5M',
+                            )
+                          ],
+                        ),
+                      )
+                  ],
+                ),
               ),
             ),
             for (var tab in tabs.skip(1))
--- lib/main.dart
+++ lib/main.dart
@@ -54,9 +54,41 @@ class TickTokApp extends StatelessWidget {
         ),
       ),
       home: const MainNavigationScreen(),
+      // home: const LayoutBuilderCodeLab(), // 메인레이아웃 역할 확인
       // home: const SignUpScreen(),
       // home: const ActivityScreen(),
       // home: const InterestsScreen(),
+    );
+  }
+}
+
+class LayoutBuilderCodeLab extends StatelessWidget {
+  const LayoutBuilderCodeLab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SizedBox(
+        width: size.width / 2,
+        // LayoutBuilder -> 자식 위젯의 최대 크기 정보를 알 수 있다.
+        child: LayoutBuilder(
+          builder: (context, constraints) => Container(
+            width: constraints.maxWidth,
+            height: constraints.maxHeight,
+            color: Colors.teal,
+            child: Center(
+              child: Text(
+                '${constraints.maxWidth} / ${size.width}',
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 98,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }

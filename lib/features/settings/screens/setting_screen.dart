d7d850279bd1d938f489a30b427b47c6cd059227
elian <elian118@naver.com>
2023-02-15 14:33:59 +0900

    13.1 AboutListTile

--- lib/features/settings/screens/setting_screen.dart
+++ lib/features/settings/screens/setting_screen.dart
@@ -6,20 +6,46 @@ class SettingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Settings'),
-        ),
-        body: Column(
-          children: const [
-            // CupertinoActivityIndicator(
-            //   radius: 40,
-            //   // animating: false,
-            // ),
-            // CircularProgressIndicator(),
-            CircularProgressIndicator.adaptive(), // 간편 쿠퍼티노 스타일 매터리얼 인디케이터 적용
-          ],
-        )
-        // body: const ListWheelScrollViewEx(),
-        );
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+      body: ListView(
+        children: const [
+          // ListTile(
+          //   onTap: () => showAboutDialog(
+          //     context: context,
+          //     applicationVersion: "v 1.0",
+          //     applicationLegalese:
+          //         "Copyright(c) All rights reserved. Please don't copy me.",
+          //   ),
+          //   title: const Text(
+          //     'About',
+          //     style: TextStyle(
+          //       fontWeight: FontWeight.w600,
+          //     ),
+          //   ),
+          //   subtitle: const Text("About this app.."),
+          // ),
+          // 위 코드를 간소화한 위젯
+          AboutListTile(
+            applicationVersion: "v 1.0",
+            applicationLegalese:
+                "Copyright(c) All rights reserved. Please don't copy me.",
+          ),
+        ],
+      ),
+      // 이하, ListWheelScrollViewEx, CupertinoActivityIndicator, CircularProgressIndicator 샘플
+      // body: Column(
+      //   children: const [
+      //     // CupertinoActivityIndicator(
+      //     //   radius: 40,
+      //     //   // animating: false,
+      //     // ),
+      //     // CircularProgressIndicator(),
+      //     CircularProgressIndicator.adaptive(), // 간편 쿠퍼티노 스타일 매터리얼 인디케이터 적용
+      //   ],
+      // ),
+      // body: const ListWheelScrollViewEx(),
+    );
   }
 }

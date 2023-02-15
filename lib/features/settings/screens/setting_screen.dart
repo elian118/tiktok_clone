1c884a8aa08cd0090bf972abe7dbec84ccef566f
elian <elian118@naver.com>
2023-02-15 15:37:17 +0900

    13.4 CupertinoAlertDialog

--- lib/features/settings/screens/setting_screen.dart
+++ lib/features/settings/screens/setting_screen.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/features/settings/widgets/about_list_tile_ex.dart';
 import 'package:tiktok_clone/features/settings/widgets/date_time_picker_ex.dart';
+import 'package:tiktok_clone/utils/utils.dart';
 
 class SettingScreen extends StatefulWidget {
   const SettingScreen({Key? key}) : super(key: key);
@@ -54,6 +57,54 @@ class _SettingScreenState extends State<SettingScreen> {
             value: _notification,
             onChanged: _onNotificationsChanged,
             title: const Text('Notification'),
+          ),
+          ListTile(
+            title: const Text('Log out (iOS)'),
+            textColor: Colors.red,
+            onTap: () {
+              showCupertinoDialog(
+                context: context,
+                builder: (context) => CupertinoAlertDialog(
+                  title: const Text('Are you sure?'),
+                  content: const Text("Please don't go"),
+                  actions: [
+                    CupertinoDialogAction(
+                      onPressed: () => Utils.navPop(context),
+                      child: const Text('No'),
+                    ),
+                    CupertinoDialogAction(
+                      onPressed: () => Utils.navPop(context),
+                      isDestructiveAction: true, // 폰트에 빨간색 입혀짐
+                      child: const Text('Yes'),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+          ListTile(
+            title: const Text('Log out (Android)'),
+            textColor: Colors.red,
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  // icon: const FaIcon(FontAwesomeIcons.skull),
+                  title: const Text('Are you sure?'),
+                  content: const Text("Please don't go"),
+                  actions: [
+                    IconButton(
+                      onPressed: () => Utils.navPop(context),
+                      icon: const FaIcon(FontAwesomeIcons.car),
+                    ),
+                    TextButton(
+                      onPressed: () => Utils.navPop(context),
+                      child: const Text('Yes'),
+                    ),
+                  ],
+                ),
+              );
+            },
           ),
           const DateTimePickerEx(),
           const AboutListTileEx(),

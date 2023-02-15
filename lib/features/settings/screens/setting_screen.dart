87b4c8e0d1d323e84238c7ed94ea5739a0fec8ea
elian <elian118@naver.com>
2023-02-15 15:50:29 +0900

    13.5 CupertinoActionSheet

--- lib/features/settings/screens/setting_screen.dart
+++ lib/features/settings/screens/setting_screen.dart
@@ -106,6 +106,36 @@ class _SettingScreenState extends State<SettingScreen> {
               );
             },
           ),
+          ListTile(
+            title: const Text('Log out (iOS / Bottom)'),
+            textColor: Colors.red,
+            onTap: () {
+              // 쿠퍼티노 모달 vs 쿠퍼티노 다이얼로그
+              //  공통: 액션버튼 텍스트가 길어지면 좌우로 꽉참
+              //  차이
+              //    1. 쿠퍼티노 모달은 다른 영역을 눌렀을 때 창이 알아서 닫힘
+              //    2. 액션버튼 텍스트가 길어져 좌우로 꽉 찬 경우 -> 모달은 아래, 다이얼로그는 위로 배치됨
+              showCupertinoModalPopup(
+                context: context,
+                builder: (context) => CupertinoActionSheet(
+                  title: const Text('Are you sure?'),
+                  message: const Text('Please dooooooon gooooo'),
+                  actions: [
+                    CupertinoActionSheetAction(
+                      isDefaultAction: true, // 기본 선택 효과 -> 폰트 굵게
+                      onPressed: () => Utils.navPop(context),
+                      child: const Text('Not log out'),
+                    ),
+                    CupertinoActionSheetAction(
+                      onPressed: () => Utils.navPop(context),
+                      isDestructiveAction: true, // 폰트에 빨간색 입혀짐
+                      child: const Text('Yes, Please.'),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
           const DateTimePickerEx(),
           const AboutListTileEx(),
           // const IndicatorEx(),

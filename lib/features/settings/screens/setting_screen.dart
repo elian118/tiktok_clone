8455694babc9eb00d051da8980c0f9f6676e6df4
elian <elian118@naver.com>
2023-02-15 15:22:02 +0900

    13.3 SwitchListTile

--- lib/features/settings/screens/setting_screen.dart
+++ lib/features/settings/screens/setting_screen.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/features/settings/widgets/about_list_tile_ex.dart';
 import 'package:tiktok_clone/features/settings/widgets/date_time_picker_ex.dart';
 
-class SettingScreen extends StatelessWidget {
+class SettingScreen extends StatefulWidget {
   const SettingScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SettingScreen> createState() => _SettingScreenState();
+}
+
+class _SettingScreenState extends State<SettingScreen> {
+  bool _notification = false;
+
+  void _onNotificationsChanged(bool? newValue) {
+    if (newValue == null) return;
+    setState(() {
+      _notification = newValue;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,10 +25,42 @@ class SettingScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Settings'),
       ),
-      body: const DateTimePickerEx(),
-      // body: const AboutListTileEx(),
-      // body: const IndicatorEx(),
-      // body: const ListWheelScrollViewEx(),
+      body: ListView(
+        children: [
+          // Switch.adaptive() = CupertinoSwitch(...)
+          // Switch.adaptive(
+          //   value: _notification,
+          //   onChanged: _onNotificationsChanged,
+          // ),
+          // Switch(
+          //   value: _notification,
+          //   onChanged: _onNotificationsChanged,
+          // ),
+          // Switch -> SwitchListTile(title, ...) -> SwitchListTile(title, ...).adaptive -> 쿠퍼티노 스타일 스위치리스트타일
+          SwitchListTile.adaptive(
+            value: _notification,
+            onChanged: _onNotificationsChanged,
+            title: const Text('Enable notifications'),
+            subtitle: const Text('Enable notifications'),
+          ),
+          // Checkbox(
+          //   value: _notification,
+          //   onChanged: _onNotificationsChanged,
+          // ),
+          // Checkbox -> CheckboxListTile(title, ...)
+          CheckboxListTile(
+            activeColor: Theme.of(context).primaryColor,
+            // checkColor: Theme.of(context).primaryColor,
+            value: _notification,
+            onChanged: _onNotificationsChanged,
+            title: const Text('Notification'),
+          ),
+          const DateTimePickerEx(),
+          const AboutListTileEx(),
+          // const IndicatorEx(),
+          // const ListWheelScrollViewEx(),
+        ],
+      ),
     );
   }
 }

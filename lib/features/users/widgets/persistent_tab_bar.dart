9ce7fe778cda49857f3afbd75d5bf2cf0fc4b06e
elian <elian118@naver.com>
2023-02-15 10:56:15 +0900

    12.6 PersistentTabBar

---
+++ lib/features/users/widgets/persistent_tab_bar.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/common/constants/sizes.dart';
+
+class PersistentTabBar extends SliverPersistentHeaderDelegate {
+  @override
+  Widget build(
+    BuildContext context,
+    double shrinkOffset,
+    bool overlapsContent,
+  ) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.symmetric(
+          horizontal: BorderSide(
+            color: Colors.grey.shade200,
+            width: 0.5,
+          ),
+        ),
+      ),
+      child: const TabBar(
+        indicatorSize: TabBarIndicatorSize.label,
+        labelPadding: EdgeInsets.symmetric(
+          vertical: Sizes.size10,
+        ),
+        labelColor: Colors.black,
+        indicatorColor: Colors.black,
+        tabs: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: Sizes.size20),
+            child: Icon(Icons.grid_4x4_rounded),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: Sizes.size20),
+            child: FaIcon(FontAwesomeIcons.heart),
+          ),
+        ],
+      ),
+    );
+  }
+
+  /*
+  maxExtent, minExtent 값이 실제 그려질 크기를 초과할 경우 예외 발생
+  다트 콘솔 예외 메시지를 확인하고 요구되는 수치를 직접 입력
+
+  SliverGeometry is not valid: The "layoutExtent" exceeds the "paintExtent".
+
+  The paintExtent is 47.0, but the layoutExtent is 200.0. // 이 부분
+  */
+  @override
+  double get maxExtent => 47;
+
+  @override
+  double get minExtent => 47;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return false;
+  }
+}

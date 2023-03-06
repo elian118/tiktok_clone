4582bce49e49c627a23c6bd779a37c7e42a8ff4f
elian <elian118@naver.com>
2023-03-06 16:57:30 +0900

    22.1 NotifierProvider

--- lib/features/videos/view_models/playback_config_vm.dart
+++ lib/features/videos/view_models/playback_config_vm.dart
@@ -9,16 +9,22 @@ class PlaybackConfigViewModel extends Notifier<PlaybackConfigModel> {
 
   void setMuted(bool value) {
     _repository.setMuted(value);
-    // build() 리턴값이 get state{}에 담겨 전달 => state 재정의(setState)
-    state = PlaybackConfigModel(muted: value, autoplay: state.autoplay);
+    // state 재정의(setState 유사)
+    state = PlaybackConfigModel(
+      muted: value,
+      autoplay: state.autoplay, // build() 리턴값이 get state{}에 담겨 전달
+    );
   }
 
   void setAutoplay(bool value) {
     _repository.setAutoplay(value);
-    state = PlaybackConfigModel(muted: state.muted, autoplay: value);
+    state = PlaybackConfigModel(
+      muted: state.muted,
+      autoplay: value,
+    );
   }
 
-  // Notifier.build() riverpod 설치 후 Notifier 상속 시 구현해야 하는 필수 메서드
+  // Notifier.build(): riverpod 설치 후 Notifier 상속 시 구현해야 하는 필수 메서드
   //  변경사항 감지하면 build() 재실행 => StatefulWidget 과 작동방식 동일
   @override
   build() {
@@ -34,8 +40,18 @@ class PlaybackConfigViewModel extends Notifier<PlaybackConfigModel> {
 // 외부로 export -> main.dart 에서 사용
 final playbackConfigProvider =
     NotifierProvider<PlaybackConfigViewModel, PlaybackConfigModel>(
-  /* () => PlaybackConfigViewModel() // 원래대로라면 이렇게 키 랩퍼를 매개변수 없이 넣어야 한다.
-     하지만, 이 키 랩퍼는 매개변수로 main.dart 에서만 호출 가능한 SharedPreferences 인스턴스 필요 */
-  () =>
-      throw UnimplementedError(), // 어쩔 수 없이, 여기서 예외 처리 후 main.dart 에서 오버라이드 위임
+  () => throw UnimplementedError(), // 예외 처리 후 main.dart 에서 오버라이드 위임
 );
+
+/*
+  원래대로라면 이렇게 키 랩퍼를 매개변수 없이 넣어야 한다.
+
+  final playbackConfigProvider =
+      NotifierProvider<PlaybackConfigViewModel, PlaybackConfigModel>(
+    () => PlaybackConfigViewModel(),
+  );
+
+   하지만, 이 키 랩퍼는 매개변수로 main.dart 에서만 호출 가능한 SharedPreferences 인스턴스 필요
+   -> 어쩔 수 없이, 여기서는 playbackConfigProvider 초기화가 불가능하므로,
+      예외 처리 후 import 하는 main.dart 에 오버라이드 위임
+ */
